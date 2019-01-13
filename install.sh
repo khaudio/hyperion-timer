8de588266a2115928adf92768f7370c1e1a108ee
Kyle Hughes <kylehughes1@gmail.com>
2019-01-13 12:16:50 -0600

    install.sh now checks for root and no longer restarts hyperion itself; only the installed timer service

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-systemctl stop hyperion
+if [ "$(whoami)" != "root" ]; then
+    echo "Error: Must be run as root; exiting."
+    exit
+fi
 cp hyperion-timer.service /etc/systemd/system/hyperion-timer.service
 systemctl daemon-reload
 systemctl enable hyperion-timer
-systemctl start hyperion
 systemctl start hyperion-timer
